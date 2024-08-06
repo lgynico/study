f22ce1ff11421d395eb302dafca6ad7df1663055
nico <935381009@qq.com>
2024-08-06 23:36:09 +0800

    游戏开发数学

--- 游戏开发/demo/Matrix3x3.h
+++ 游戏开发/demo/Matrix3x3.h
@@ -1,11 +1,21 @@
 #ifndef __MATRIX3X3_H__
 #define __MATRIX3X3_H__
 
+#include <algorithm>
+
 class Vector3;
 
 class Matrix3x3
 {
+private:
+    float m[9];
+
 public:
+    void set(const float _m[9])
+    {
+        std::copy(_m[0], _m[0] + 9, m[0]);
+    }
+
     float _11, _12, _13;
     float _21, _22, _23;
     float _31, _32, _33;
@@ -13,6 +23,23 @@ public:
     void rotateX(float theta);
     void rotateY(float theta);
     void rotateZ(float theta);
+
+    void scale(float sx, float sy, float sz);
+    void scale(const Vector3 &v);
+
+    void projectXZ();
+    void projectXY();
+    void projectYZ();
+    void project(const Vector3 &n);
+
+    void reflectXZ();
+    void reflectXY();
+    void reflectYZ();
+    void reflect(const Vector3 &n);
+
+    void shearXY(float s, float t);
+    void shearXZ(float s, float t);
+    void shearYZ(float s, float t);
 };
 
 Matrix3x3 operator*(const Matrix3x3 &m1, const Matrix3x3 &m2);
--- 游戏开发/demo/matrix3x3.cpp
+++ 游戏开发/demo/matrix3x3.cpp
@@ -7,6 +7,13 @@ inline void Matrix3x3::rotateX(float theta)
     float sin, cos;
     sincosf(theta, &sin, &cos);
 
+    float m[9] = {
+        1, 0, 0,
+        0, cos, sin,
+        0, -sin, cos};
+
+    set(m);
+
     _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
     _21 = 0.0f, _22 = cos, _23 = sin;
     _31 = 0.0f, _32 = -sin, _33 = cos;
@@ -69,4 +76,93 @@ Vector3 &operator*=(Vector3 &v, const Matrix3x3 &m)
 {
     Vector3 t = v * m;
     return t;
+}
+
+void Matrix3x3::scale(float sx, float sy, float sz)
+{
+    _11 = sx, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = sy, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = sz;
+}
+
+void Matrix3x3::scale(const Vector3 &v)
+{
+    scale(v.x, v.y, v.z);
+}
+
+void Matrix3x3::projectXZ()
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 0.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
+}
+
+void Matrix3x3::projectXY()
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 0.0f;
+}
+
+void Matrix3x3::projectYZ()
+{
+    _11 = 0.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
+}
+
+void Matrix3x3::project(const Vector3 &n)
+{
+    _11 = 1 - n.x * n.x, _12 = -n.x * n.y, _13 = -n.x * n.z;
+    _21 = -n.x * n.y, _22 = 1 - n.y * n.y, _23 = -n.y * n.z;
+    _31 = -n.x * n.z, _32 = -n.y * n.z, _33 = 1 - n.z * n.z;
+}
+
+void Matrix3x3::reflectXZ()
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = -1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
+}
+
+void Matrix3x3::reflectXY()
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = -1.0f;
+}
+
+void Matrix3x3::reflectYZ()
+{
+    _11 = -1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
+}
+
+void Matrix3x3::reflect(const Vector3 &n)
+{
+    _11 = 1 - 2 * n.x * n.x, _12 = -2 * n.x * n.y, _13 = -2 * n.x * n.z;
+    _21 = -2 * n.x * n.y, _22 = 1 - 2 * n.y * n.y, _23 = -2 * n.y * n.z;
+    _31 = -2 * n.x * n.z, _32 = -2 * n.y * n.z, _33 = 1 - 2 * n.z * n.z;
+}
+
+void Matrix3x3::shearXY(float s, float t)
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = s, _32 = t, _33 = 1.0f;
+}
+
+void Matrix3x3::shearXZ(float s, float t)
+{
+    _11 = 1.0f, _12 = 0.0f, _13 = 0.0f;
+    _21 = s, _22 = 1.0f, _23 = t;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
+}
+
+void Matrix3x3::shearYZ(float s, float t)
+{
+    _11 = 1.0f, _12 = s, _13 = t;
+    _21 = 0.0f, _22 = 1.0f, _23 = 0.0f;
+    _31 = 0.0f, _32 = 0.0f, _33 = 1.0f;
 }
